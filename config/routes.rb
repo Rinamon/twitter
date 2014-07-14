d7fa7903c40c6205e16b523eb5273539784cc00f
Rinat <rinat_turganbekov@yahoo.com>
2014-07-14 09:54:12 +0000

    FInal

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do  
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :microposts, only: [:create, :destroy]
   get "users/new"
   
